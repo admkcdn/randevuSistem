27341c0091db5a58774072e77b6ac5d8792912d3
admkcdn <admkcdn@gmail.com>
2022-05-24 16:38:16 +0300

    Add project files.

---
+++ randevuSistem/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace randevuSistem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(113, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(178, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(178, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(113, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soy İsim";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(178, 217);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(113, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefon";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(116, 285);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.Value = new System.DateTime(2022, 3, 21, 0, 0, 0, 0);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(163, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(163, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 48);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Randevu Defteri";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(6, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(379, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "SIR ADEM KOÇDOĞAN İLE GÖRÜŞMEK";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(102, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(217, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "İÇİN RANDEVU ALINIZ!";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic);
+            this.linkLabel1.Location = new System.Drawing.Point(53, 460);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(284, 16);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Randevu gününe kadar Adem\'i yakından tanıyın!";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(392, 496);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ADEM RANDEVU SİSTEMİ (ARS)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ randevuSistem/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace randevuSistem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 form2 = new Form2();
+        List<Dictionary<string, string>> rows = new List<Dictionary<string, string>>();
+        List<Dictionary<string, string>> rows2 = new List<Dictionary<string, string>>();
+        Dictionary<string, string> column;
+        Dictionary<string, string> randevular;
+        static string constring = "Data Source=localhost;Initial Catalog=Kisiler;Integrated Security=True"; 
+        SqlConnection connect = new SqlConnection(constring);
+        string sqlQuery = "SELECT id,adi,soyadi,telefon,randevuTarih FROM Bilgi \n order by randevuTarih";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                if (connect.State == ConnectionState.Closed)
+                {
+                    connect.Open();
+                    string tarihsql = "select randevuTarih FROM Bilgi";
+                    SqlCommand command = new SqlCommand(tarihsql, connect);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        randevular = new Dictionary<string, string>();
+                        randevular["randevuTarih"] = reader["randevuTarih"].ToString();
+                        rows2.Add(randevular);
+                    }
+                    reader.Close();
+
+                    int counter = 0;
+
+                    foreach(Dictionary<string,string> tarihler in rows2)
+                    {
+                        if (tarihler["randevuTarih"].Remove(10) == dateTimePicker1.Value.Date.ToString().Remove(10))
+                            counter++;
+                    }
+
+                    if (counter == 0)
+                        { 
+                        string kayit = "insert into Bilgi (adi,soyadi,telefon,randevuTarih) values (@ad,@soyad,@telefon,@tarih)";
+                        SqlCommand cmd = new SqlCommand(kayit, connect);
+                        cmd.Parameters.AddWithValue("@ad", textBox1.Text);
+                        cmd.Parameters.AddWithValue("@soyad", textBox2.Text);
+                        cmd.Parameters.AddWithValue("@telefon", textBox3.Text);
+                        cmd.Parameters.AddWithValue("@tarih", dateTimePicker1.Value);
+
+                        cmd.ExecuteNonQuery();
+
+                        connect.Close();
+                        MessageBox.Show("Başarıyla Randevu alındı!");
+                        }
+                    else
+                    {
+                        MessageBox.Show("Aynı güne birden fazla randevu verilemez!");
+                        connect.Close();
+                    }
+                    }
+                }
+            catch(Exception err) {
+                MessageBox.Show("Hata: "+err);
+                connect.Close();
+                }
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand(sqlQuery, connect);
+            try
+            {
+                connect.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                while (reader.Read())
+                {  
+                    column = new Dictionary<string, string>();
+
+                    column["id"] = reader["id"].ToString();
+                    column["adi"] = reader["adi"].ToString();
+                    column["soyadi"] = reader["soyadi"].ToString();
+                    column["telefon"] = reader["telefon"].ToString();
+                    column["randevuTarih"] = reader["randevuTarih"].ToString();
+
+                    rows.Add(column);
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Hata: "+ ex);
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+
+            foreach (Dictionary<string, string> column in rows)
+            {
+                form2.listBox1.Items.Add(column["id"] + "\t" + column["adi"] + "\t" + column["soyadi"] + "\t" + column["telefon"] + "\t" + column["randevuTarih"].Remove(10));
+            }
+            form2.Show();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://ademkocdogan.wordpress.com/");
+        }
+    }
+    }
+
